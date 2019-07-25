a16717cd27639389b3d70a81d87bed5cf7ea7131
Engin Polat <enpolat@microsoft.com>
2019-07-25 16:53:38 +0300

    publish script changed to use master arm template

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,5 @@
-az group deployment create --resource-group t4m --template-file serverfarm.json
+az group deployment create --resource-group t4m --template-file master.json
 
-az group deployment create --resource-group t4m --template-file function.json
+# az group deployment create --resource-group t4m --template-file serverfarm.json
+
+# az group deployment create --resource-group t4m --template-file function.json
