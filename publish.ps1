3c557fd7c77c28d21c803f56163f7ec60532cfd8
Engin Polat <enpolat@microsoft.com>
2019-07-25 17:20:44 +0300

    now it's publishing the project as well

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,19 @@
-az group deployment create --resource-group t4m --template-file master.json
+$resourceGroup = "testing"
+$functionAppName = "functest20190725"
+$count = 4
 
-# az group deployment create --resource-group t4m --template-file serverfarm.json
+az group deployment create --resource-group $resourceGroup --template-file master.json --parameters functionName=$functionAppName count=$count
 
-# az group deployment create --resource-group t4m --template-file function.json
+cd appcode
+
+dotnet publish -c Release
+$publishFolder = "./appcode/bin/Release/netcoreapp2.1/publish"
+
+$publishZip = "publish.zip"
+if (Test-path $publishZip) { Remove-item $publishZip }
+
+Add-Type -assembly "system.io.compression.filesystem"
+
+[io.compression.zipfile]::CreateFromDirectory($publishFolder, $publishZip)
+
+az functionapp deployment source config-zip -g $resourceGroup -n $functionAppName --src $publishZip
